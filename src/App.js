42143e457b013317db4efbb5c175c3a675dd8ad6
joaomarcossandes <joaodeitabi@hotmail.com>
2026-01-31 10:34:25 -0300

    Adiciona Navbar e Footer ao layout

--- src/App.js
+++ src/App.js
@@ -3,24 +3,27 @@ import Home from "./components/pages/Home";
 import Contact from "./components/pages/Contact";
 import Company from "./components/pages/Company";
 import NewProject from "./components/pages/NewProject";
+import Projects from "./components/pages/Projects";
+
+import Container from "./components/layout/Container";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer"
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <div>
-          <Link to="/">Home</Link>
-          <Link to="/contact">Contact</Link>
-          <Link to="/company">Company</Link>
-          <Link to="/newproject">NewProject</Link>
-        </div>
-        <Routes>
-          <Route path="/" element={ <Home />} />
-          <Route path="/company" element={ <Company />} />
-          <Route path="/contact" element={ <Contact />} />
-          <Route path="/newproject" element={ <NewProject />} />
-        </Routes>
-        <p>Footer</p>
+        <Navbar />
+          <Container customClass="min-height">
+            <Routes>
+                <Route path="/" element={ <Home />} />
+                <Route path="/projects" element={ <Projects />} />
+                <Route path="/company" element={ <Company />} />
+                <Route path="/contact" element={ <Contact />} />
+                <Route path="/newproject" element={ <NewProject />} />
+            </Routes>
+          </Container>
+        <Footer />
       </Router>
       
     </div>
---
+++ src/components/layout/Footer.js
@@ -0,0 +1,29 @@
+﻿import { FaFacebook } from 'react-icons/fa'
+import { FaInstagram } from 'react-icons/fa'
+import { FaLinkedin } from 'react-icons/fa'
+
+import styles from './Footer.module.css'
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <ul className={styles.social_list}>
+                <li>
+                    <FaFacebook />
+                </li>
+                <li>
+                    <FaInstagram />
+                </li>
+                <li>
+                    <FaLinkedin />
+                </li>                
+            </ul>
+            <p>
+                <span>Costs</span> &copy; 2026
+            </p>
+        </footer>
+    )
+    
+}
+
+export default Footer
